2c25181351c7b00ae78278cfc66912d95cc83572
xiaohua0877 <xiaohua0877@sina.com>
2019-09-14 12:07:22 +0800

    进一步优化 名称

--- txt_open/memory_map_of_the_image.py
+++ txt_open/memory_map_of_the_image.py
@@ -11,36 +11,40 @@ def is_number(s):
         return True
     except ValueError:
         pass
-
     try:
         import unicodedata
         unicodedata.numeric(s)
         return True
     except (TypeError, ValueError):
         pass
-
     return False
 
 
-# def test_code_write(b):
-#     wb = load_workbook('flash.xlsx')
-#     wb.guess_types = True
-#     ws = wb.active
-#     ws['A1'] = 42
-#     ws.append([1, 2, 3])
-#     src_list = b
-#     for bb in src_list:
-#         new_number = []
-#         for n in bb:
-#             if is_number(n) is True:
-#                 new_number.append(int(n))
-#             else:
-#                 new_number.append(n)
-#         bb = new_number
-#         ws.append(bb)
-#     wb.save("flash.xlsx")
+def write_Memory_Map_data(bbList):
+    now = datetime.datetime.now()
+    otherStyleTime = now.strftime("%Y_%m_%d_%H%M%S")
+    print(otherStyleTime)
+    xlsx_name = 'out\\' + otherStyleTime + 'Memory_Map.xlsx'
+    wb = Workbook()  # 创建文件对象
+    ws = wb.active  # 获取第一个sheet
+    ws['A1'] = 42
+    ws.append([1, 2, 3])
+    src_list = bbList
+    for bb in src_list:
+        new_number = []
+        for n in bb:
+            if is_number(n) is True:
+                new_number.append(int(n))
+            else:
+                new_number.append(n)
+        bb = new_number
+        ws.append(bb)
 
-#Image component sizes
+    # Save the file
+    wb.save(xlsx_name)
+
+
+# Image component sizes
 def open_Memory_Map_of_image():
     # 第一种方法
     f = open("flash.map", "r")  # 设置文件对象
@@ -66,11 +70,14 @@ def open_Memory_Map_of_image():
             if 'Execution Region' in line:
                 continue
             if 'Exec Addr' in line:
-                result = ['Exec Addr','Load Addr','Size','Type','Attr','Idx','E Section Name','Object']
+                result = []
+                Image_list.append(result)
+                Image_list.append(result)
+                result = ['Exec Addr', 'Load Addr', 'Size', 'Type', 'Attr', 'Idx', 'E Section Name', 'Object']
             else:
                 print('第%s行:%s' % (count, line))
                 result = line.split()
-                print(result)
+                #print(result)
             Image_list.append(result)
 
     f.close()  # 关闭文件
@@ -79,9 +86,7 @@ def open_Memory_Map_of_image():
 
 
 Image_list = open_Memory_Map_of_image()
-print(Image_list)
+write_Memory_Map_data(Image_list)
 
 # Image_list = open_image_component_sizes()
 # test_code_write(Image_list)
-
-
--- txt_open/open_image_comm.py
+++ txt_open/open_image_comm.py
@@ -4,11 +4,12 @@ from openpyxl import Workbook
 from openpyxl import load_workbook
 import time, datetime
 
+
 def my_test_code_str():
     now = datetime.datetime.now()
     otherStyleTime = now.strftime("%Y_%m_%d_ %H%M%S")
     print(otherStyleTime)
-    xlsx_name = 'out\\'+otherStyleTime + 'test.xlsx'
+    xlsx_name = 'out\\' + otherStyleTime + 'test.xlsx'
     print(xlsx_name)
     wb = Workbook()  # 创建文件对象
     # grab the active worksheet
@@ -25,7 +26,7 @@ def my_test_code_str():
     ws.append(b)
 
     line = '0x080001d8   0x080001d8   0x00000000   Code   RO         2843    .ARM.Collect$$$$0000000F  mc_w.l(entry11a.o)\n'
-    line= line[:-1]
+    line = line[:-1]
     b = line.split()
     print(b)
     ws.append(b)
@@ -60,15 +61,16 @@ def is_number(s):
     return False
 
 
-def write_image_code(bbList):
+def create_my_timename():
     now = datetime.datetime.now()
-    otherStyleTime = now.strftime("%Y_%m_%d_ %H%M%S")
-    print(otherStyleTime)
-    xlsx_name = 'out\\' + otherStyleTime + 'image_comm.xlsx'
+    otherStyleTime = now.strftime("%Y_%m_%d_%H%M%S")
+    return otherStyleTime
+
+
+def write_image_code(bbList, image_comm_name):
+    xlsx_name = image_comm_name
     wb = Workbook()  # 创建文件对象
     ws = wb.active  # 获取第一个sheet
-    ws['A1'] = 42
-    ws.append([1, 2, 3])
     src_list = bbList
     for bb in src_list:
         new_number = []
@@ -79,11 +81,27 @@ def write_image_code(bbList):
                 new_number.append(n)
         bb = new_number
         ws.append(bb)
-
     # Save the file
     wb.save(xlsx_name)
+    print('保存文件名 ' + xlsx_name)
 
-#Image component sizes
+
+def List_to_7Item(src_list):
+    # print(src_list)
+    tt = 0
+    list_dst = []
+    for n in src_list:
+        tt += 1
+        if tt > 7:
+            # print(result1[6])
+            list_dst[6] = list_dst[6] + ' ' + n
+        else:
+            list_dst.append(n)
+    #print(list_dst)
+    return list_dst
+
+
+# Image component sizes
 def open_image_component_sizes():
     # 第一种方法
     f = open("flash.map", "r")  # 设置文件对象
@@ -92,6 +110,7 @@ def open_image_component_sizes():
     count = 1
     Image_start_flag = 0
     Image_com_list = []
+    space_line = []
     while src_line:  # 直到读取完文件
         src_line = f.readline()  # 读取一行文件，包括换行符
         count += 1
@@ -105,21 +124,26 @@ def open_image_component_sizes():
             if 'Total R' in line:
                 continue
             if 'Code (inc. data)' in line:
-                result = []
-                Image_com_list.append(result)
-                Image_com_list.append(result)
+                Image_com_list.append(space_line)
+                Image_com_list.append(space_line)
                 result = ['Code(inc)', 'data', 'RO Data', 'RW Data', 'ZI Data', 'Debug', 'Object Name']
             else:
-                print('第%s行:%s' % (count, line))
+                #print('第%s行:%s' % (count, line))
                 result = line.split()
-                print(result)
+
+                if line.endswith('Object Totals'):
+                    Image_com_list.append(space_line)
+                    Image_com_list.append(space_line)
+                if len(result) > 7:
+                    result = List_to_7Item(result)
             Image_com_list.append(result)
 
     f.close()  # 关闭文件
     return Image_com_list
 
 
-my_test_code_str()
+# xlsx_name = 'out\\' + otherStyleTime + 'image_comm.xlsx'
+image_comm_name = 'out\\' + create_my_timename() + '_image_comm.xlsx'
+# my_test_code_str()
 image_com_list = open_image_component_sizes()
-write_image_code(image_com_list)
-
+write_image_code(image_com_list, image_comm_name)
